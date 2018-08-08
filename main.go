ff107d3d27e0c687ae25f5a8dc9cbaae30522cfe
Kasper Olsen <root@kols.dk>
2018-08-08 13:43:36 +0200

    lied in prev commit lol - now added support for raspberry pi deployment

--- main.go
+++ main.go
@@ -5,17 +5,28 @@ import (
 	"time"
 
 	"github.com/kelvins/sunrisesunset"
+	rpio "github.com/stianeikeland/go-rpio"
 )
 
 func main() {
+	rpioErr := rpio.Open()
+	pin := rpio.Pin(2)
+	pin.Output()
 	isOn := false
 	for {
+		if rpioErr == nil {
+			fmt.Println(rpioErr)
+			rpio.Close()
+			break
+		}
+
 		param := sunrisesunset.Parameters{
 			Latitude:  55.566935,
 			Longitude: 12.245406,
 			UtcOffset: 2.0,
 			Date:      time.Now(),
 		}
+
 		sunrise, sunset, err := param.GetSunriseSunset()
 		now := time.Date(1, 1, 1, time.Now().Hour(), time.Now().Minute(), time.Now().Second(), 0, time.UTC)
 		tdySunset := time.Date(1, 1, 1, sunset.Hour(), sunset.Minute(), sunset.Second(), 0, time.UTC)
@@ -29,14 +40,17 @@ func main() {
 
 			if now.After(tdySunset) && now.Before(tmrSunrise) {
 				isOn = true
+				pin.High()
 				fmt.Printf("Is on?: %t\n", isOn)
 			} else {
 				isOn = false
+				pin.Low()
 				fmt.Printf("Is on?: %t\n", isOn)
 			}
 
 		} else {
 			fmt.Println(err)
+			rpio.Close()
 			break
 		}
 		time.Sleep(time.Minute * 5)
