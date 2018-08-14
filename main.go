26e2bd675c0c90f2d7426165efe4e260499280a9
Kasper Olsen <root@kols.dk>
2018-08-14 18:08:00 +0200

    more fixes

--- main.go
+++ main.go
@@ -2,31 +2,37 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/kelvins/sunrisesunset"
-	rpio "github.com/stianeikeland/go-rpio"
+	"github.com/stianeikeland/go-rpio"
+)
+
+var (
+	pin               = rpio.Pin(4)
+	calculationParams = sunrisesunset.Parameters{
+		Latitude:  55.566935,
+		Longitude: 12.245406,
+		UtcOffset: 2.0,
+		Date:      time.Now(),
+	}
 )
 
 func main() {
 	rpioErr := rpio.Open()
-	pin := rpio.Pin(4)
+	if rpioErr != nil {
+		fmt.Println(rpioErr)
+		os.Exit(1)
+	}
+
+	defer rpio.Close()
 	pin.Output()
+
 	for {
-		if rpioErr != nil {
-			fmt.Println(rpioErr)
-			rpio.Close()
-			break
-		}
 
-		param := sunrisesunset.Parameters{
-			Latitude:  55.566935,
-			Longitude: 12.245406,
-			UtcOffset: 2.0,
-			Date:      time.Now(),
-		}
+		sunrise, sunset, err := calculationParams.GetSunriseSunset()
 
-		sunrise, sunset, err := param.GetSunriseSunset()
 		now := time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day(), time.Now().Hour(), time.Now().Minute(), time.Now().Second(), 0, time.UTC)
 		// now := time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day(), 14, 12, 13, 0, time.UTC)
 		tdySunset := time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day(), sunset.Hour(), sunset.Minute(), sunset.Second(), 0, time.UTC)
@@ -40,14 +46,15 @@ func main() {
 
 			if (now.After(tdySunset) && now.Before(tmrSunrise.Add(time.Hour*24))) || (now.Before(tdySunset.Add(time.Hour*24)) && now.Before(tmrSunrise)) {
 				pin.High()
+				fmt.Println("Light is on")
 			} else {
+				fmt.Println("Light is off")
 				pin.Low()
 			}
 
 		} else {
 			fmt.Println(err)
-			rpio.Close()
-			break
+			os.Exit(1)
 		}
 		time.Sleep(time.Minute * 5)
 
